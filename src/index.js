216f9bf8d6e3fce226e19a446e310fd6ed385c6c
VitalyOvsiyenko <ovsiyenko80@gmail.com>
2023-09-13 11:29:35 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/cv_full-stack_developer">
+    <BrowserRouter basename="/CV-Full-Stack-Developer">
       <App />
     </BrowserRouter>
   </React.StrictMode>
