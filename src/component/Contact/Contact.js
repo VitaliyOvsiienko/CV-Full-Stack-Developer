daad2c71f7cbf92c2eec4a9fece8a52eade20ec3
VitalyOvsiyenko <ovsiyenko80@gmail.com>
2023-10-02 09:21:53 +0300

    Update Contact.js

--- src/component/Contact/Contact.js
+++ src/component/Contact/Contact.js
@@ -41,7 +41,7 @@ const Contact = ({ open = false }) => {
           </ContactLink>
 
           <ContactLink
-            href="https://drive.google.com/file/d/1rEEFENL6qeWqxrxYeP5ZByFI2RtGRqJQ/view?usp=drive_link"
+            href="https://drive.google.com/file/d/1cNV0oOMFLsLdOTRei3C6E7F5KERl44_5/view?usp=drive_link"
             target="blank"
             rel="noopener noreferrer"
           >
